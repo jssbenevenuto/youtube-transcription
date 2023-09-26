14de47da4a008c47e2d746ebcd1e02c1dc08e20b
Jesse Benevenuto <jssbenevenuto@gmail.com>
2023-09-26 19:26:44 -0300

    convert video with ffmpeg

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { downloader } from './download-video.js'
+import { createMP3 } from './create-mp3.js'
 
 const app = express()
 
@@ -11,15 +12,12 @@ app.get('/audio', async (req, res) => {
 
     try {
         await downloader(videoId)
-
+        await createMP3()
         return res.send('Okay')
     } catch(error) {
         console.log(error)
         return res.send(error)
     }
-
-    console.log(videoId)
-    res.send(videoId)
 })
 
 app.listen(3333, () => console.log('Server up 🔥'))
